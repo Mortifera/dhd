096214949b2cb9ff1b6e5a200426bae298369247
Jack Gardner <Mortifera@users.noreply.github.com>
2021-01-12 21:44:24 +0000

    Adding password table

--- packages/DogHouseDiamondInfra/lib/DHDStack.ts
+++ packages/DogHouseDiamondInfra/lib/DHDStack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'monocdk';
 import * as s3 from 'monocdk/aws-s3';
 import * as iam from 'monocdk/aws-iam';
+import * as ddb from 'monocdk/aws-dynamodb';
+import { assert } from 'console';
 
 export class DHDStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,10 +12,25 @@ export class DHDStack extends cdk.Stack {
             encryption: s3.BucketEncryption.S3_MANAGED
         });
 
+        const passTable = new ddb.Table(this, "PassTable", {
+            encryption: ddb.TableEncryption.CUSTOMER_MANAGED,
+            partitionKey: {
+                name: "service",
+                type: ddb.AttributeType.STRING
+            }
+        });
+
         const user = new iam.User(this, "DataSyncUser");
 
         bucket.grantPut(user);
         bucket.grantRead(user);
+
+        passTable.grantReadData(user);
+        passTable.grantWriteData(user);
+
+        assert(passTable.encryptionKey);
+
+        passTable.encryptionKey?.grantEncryptDecrypt(user);
 
         new cdk.CfnOutput(this, "BucketArn", {
             value: bucket.bucketArn,
